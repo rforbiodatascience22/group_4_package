f6b54374409717caa0bcf3ab1b7e882af8b39da7
MartinaVojtkova <s212586@dtu.dk>
2022-03-21 09:25:30 +0000

    Finally creating the function

--- R/generate_DNA.R
+++ R/generate_DNA.R
@@ -9,7 +9,7 @@
 #' @examples
 #'
 #' generate_DNA(125)
-DNA_seq <- function(seq_size){
+generate_DNA <- function(seq_size){
   random_bases <- sample(c("A", "T", "G", "C"), size = seq_size, replace = TRUE)
   complete_seq <- paste0(random_bases, collapse = "")
   return(complete_seq)
