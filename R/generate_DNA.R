c29504564983c0483d13de01c65941c7278201ed
MartinaVojtkova <s212586@dtu.dk>
2022-03-21 09:19:24 +0000

    another try

--- R/generate_DNA.R
+++ R/generate_DNA.R
@@ -1,11 +1,14 @@
-#' DNA sequence generator
+
+#' Generate DNA sequence
 #'
 #' @param seq_size
 #'
 #' @return complete_seq
 #' @export
 #'
-#'@example generate_DNA(150)
+#' @examples
+#'
+#' generate_DNA(125)
 DNA_seq <- function(seq_size){
   random_bases <- sample(c("A", "T", "G", "C"), size = seq_size, replace = TRUE)
   complete_seq <- paste0(random_bases, collapse = "")
